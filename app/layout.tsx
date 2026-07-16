db2dee064cca6e87057f555fcdf81fa5f9494c08
Noor <chunawalanoor@gmail.com>
2026-07-16 15:04:48 +0400

    Meta tags changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export const metadata: Metadata = {
   type: "website",
   images: [
     {
-      url: "/FooterLogo.png",
+      url: "/og-image.png",
       width: 1200,
       height: 630,
       alt: "Scoolyx",
